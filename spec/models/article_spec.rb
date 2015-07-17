65d112a8996a32cc2d200ad28a8fce32b74dcd07
Ben Wootten <benmwootten@gmail.com>
2015-07-17 18:33:22 -0500

    adds a negative case test to Article#is_similar_to?

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Article, :type => :model do
 
   before :each do
     DatabaseCleaner.start
-    @article = FactoryGirl.create(:article)
+    @article = FactoryGirl.create(:article, title: 'Letting Our Emojis Get in the Way')
   end
 
   after :each do
@@ -29,6 +29,13 @@ RSpec.describe Article, :type => :model do
         expect(@article.is_similar_to?(FactoryGirl.create(:article, title: title))).to be(true)
       end
     end
-    it 'should not be similar to articles on different stories'
+    it 'should not be similar to articles on different stories' do
+      titles = ['Pepsi Celebrates World Emoji Day With More Emoji',
+                'Cats make the best pets, Bono says',
+                'Emoji Aren\'t Silly—They Could Actually Help the Deaf']
+      titles.each do |title|
+        expect(@article.is_similar_to?(FactoryGirl.create(:article, title: title))).to be(false)
+      end
+    end
   end
 end
