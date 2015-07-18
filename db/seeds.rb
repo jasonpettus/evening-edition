fc1fcccd14fa19edbfb8225f27e33972aae36ae1
Ben Wootten <benmwootten@gmail.com>
2015-07-18 18:04:57 -0500

    fixes auth stuff with secton controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+default_section = Section.new(title: Default)
+
+default_section.subscriptions.build(set_feed: 'http://america.aljazeera.com/content/ajam/articles.rss', title: 'Al Jazeera America')
+default_section.subscriptions.build(set_feed: 'http://feeds.bbci.co.uk/news/rss.xml', title: 'BBC Top Stories')
+default_section.subscriptions.build(set_feed: 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', title: 'NYTimes US')
+
