d2d445fca8b15d1963ca3f063d3d87c34d1479cc
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-18 12:30:56 -0500

    Add conditionals in Subscription#strip_ads

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -22,30 +22,34 @@ class Subscription < ActiveRecord::Base
 		@entries.each { |entry| self.articles << entry }
 	end
 
+	def get_feature_imgs
+		feature_imgs = []
+		img_areas = []
+		@entries.map do |entry|
+			Nokogiri::HTML(open(entry.url)).css('img').each do |node|
+				feature_imgs << node.attr('src')
+				img_areas << FastImage.size(node.attr('src')).reduce(1) { |length, width| length * width }
+			end
+			largest = img_areas.index(img_areas.max)
+			
+		end
+	end
+
 	private
 		def save_articles
 			strip_ads
 			@entries.each { |article| article.save }
 		end
 
-		def get_feature_imgs
-			feature_imgs = []
-			img_areas = []
-			@entries.map do |entry|
-				Nokogiri::HTML(open(entry.url)).css('img').each do |node|
-					feature_imgs << node.attr('src')
-					img_areas << FastImage.size(node.attr('src')).reduce(1) { |length, width| length * width }
-				end
-				largest = img_areas.index(img_areas.max)
-				
-			end
-		end
-
 		def strip_ads
 			@entries.each do |entry|
-				no_images = entry.summary.gsub!(/<img.*?>/,"")
-				no_links = no_images.gsub!(/<a.*?<\/a>/,"")
-				no_links.gsub!(/<br.*?>/,"")
+				if entry.summary.include? "<img"
+					no_images = entry.summary.gsub!(/<img.*?>/,"")
+				elsif entry.summary.include? "</a>"
+					no_links = no_images.gsub!(/<a.*?<\/a>/,"")
+				elsif entry.summary.include? "<br"
+					no_links.gsub!(/<br.*?>/,"")
+				end
 			end
 		end
 end
