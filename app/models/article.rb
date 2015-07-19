0125a472a026675c4d13909f15387e43a38cd1bf
Ben Wootten <benmwootten@gmail.com>
2015-07-19 17:50:20 -0500

    changes clustering weight to be more selective, some fixes for the small, big and splash templates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,26 @@
 class Article < ActiveRecord::Base
 	belongs_to	:subscription
-  SIMILARITY_WEIGHT = 0.4
+  SIMILARITY_WEIGHT = 0.45
+  LIST_OF_WORDS_TO_IGNORE = ['a','an','the','to','and','of','be', 'in', 'at', 'this', 'that']
 
-	def set_article=(article)
-		self.title = article.title
-		self.url = article.url
-		self.last_modified = article.last_modified
-		if article.summary
-			self.summary = article.summary
-		end
-	end
+  def set_article=(article)
+    self.title = article.title
+    self.url = article.url
+    self.last_modified = article.last_modified
+    if article.summary
+      self.summary = article.summary
+    end
+  end
 
   def is_similar_to?(article)
-    Text::WhiteSimilarity.new.similarity(title, article.title) >= SIMILARITY_WEIGHT
+    subbed_title = remove_ignored_words(title)
+    subbed_article_title = remove_ignored_words(article.title)
+
+    Text::WhiteSimilarity.new.similarity(subbed_title, subbed_article_title) >= SIMILARITY_WEIGHT
   end
+
+  private
+    def remove_ignored_words(string)
+      (string.downcase.split(' ') - LIST_OF_WORDS_TO_IGNORE).join(' ')
+    end
 end
