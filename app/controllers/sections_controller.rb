289035b248e1b8913716f135a09f237759dd0734
Jason Pettus <ilikejason@gmail.com>
2015-07-19 11:14:47 -0500

    Update sections_controller.rb

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,9 +5,9 @@ class SectionsController < ApplicationController
 
 	def show
     if user_logged_in?
-  		@active_section = Section.find(params[:id])
-      @sections = current_user.sections
-  		@stories = @section.stories
+  	@active_section = Section.find(params[:id])
+      	@sections = current_user.sections
+  	@stories = @section.stories
     else
       @section = Section.find_by(title: 'default')
       @stories = @section.stories
