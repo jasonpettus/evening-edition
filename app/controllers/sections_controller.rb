22ec9b031049a1cf7733fa47319c36e101c3efa5
Jason Pettus <ilikejason@gmail.com>
2015-07-20 13:17:35 -0500

    Add sections/index and sections/edit files and corresponding code

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,13 +1,18 @@
 class SectionsController < ApplicationController
 
   before_action :authorize_user_logged_in, except: :show
+  def index
+    @sections = current_user.sections
+    @section = Section.new
+    @page_name = "Edit Sections"
+  end
 
-
-	def show
+  def show
     if user_logged_in?
-    	@active_section = Section.find_by(id: params[:id]) || Section.find_by(title: 'Default')
-    	@sections = current_user.sections
+      @active_section = Section.find_by(id: params[:id]) || Section.find_by(title: 'Default')
+      @sections = current_user.sections
     	@stories = @active_section.stories
+      @page_name = @active_section.title
     else
       @section = Section.find_by(title: 'Default')
       @stories = @section.stories
@@ -18,31 +23,34 @@ class SectionsController < ApplicationController
 	end
 
   def edit
+    @sections = current_user.sections
     @section = Section.find(params[:id])
+    @page_name = "Rename Section"
   end
 
   def update
     @section = Section.find(params[:id])
-    if @section.update_attibutes(section_params)
-      redirect_to :show, id: @section.id
+    if @section.update_attributes(section_params)
+      redirect_to :back
     else
       render :edit
     end
   end
 
-  def delete
+  def destroy
     Section.find(params[:id]).destroy
-    redirect_to :show, id: user_default_section.id
+    redirect_to :back
   end
 
   def new
     @section = Section.new
+    redirect_to root_path #remove this later
   end
 
   def create
     @section = current_user.sections.build(section_params)
     if @section.save
-      redirect_to :show, id: @section.id
+      redirect_to :back
     else
       render :new
     end
@@ -50,7 +58,7 @@ class SectionsController < ApplicationController
 
   private
     def section_params
-      params.require(:sections).permit(:title)
+      params.require(:section).permit(:title)
     end
 
     def user_default_section
