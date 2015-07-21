e3e596c2a3b847536bc193350d0098bbd7d403f8
Ben Wootten <benmwootten@gmail.com>
2015-07-21 09:45:48 -0500

    ajaxs the create a free account link

--- app/models/section.rb
+++ app/models/section.rb
@@ -15,7 +15,7 @@ class Section < ActiveRecord::Base
   end
 
   def todays_stories
-    recent_stories = stories.where("stories.updated_at > now() - interval '1 day'").order("stories.id DESC")
+    recent_stories = stories.where("stories.updated_at > now() - interval '1 day'").order("stories.id DESC").limit(14)
     assign_sizes(order_stories(recent_stories))
   end
 
