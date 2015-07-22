a7766e092e418a8904945bbd981e125fb7c6b199
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-07-22 16:45:22 -0500

    Use #assign_feeds

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,7 +7,8 @@ class SubscriptionsController < ApplicationController
 		p params
 		@section = Section.find_by(id: params[:section_id])
 		@subscription = Subscription.create(subscription_params)
-		@subscription.update_attributes(section: @section, feed: Feed.first_or_create(feed_url: params['subscription']['feed']))
+		@feed = Feed.create(feed_url: params['subscription']['feed'])
+		@subscription.update_attributes(section: @section, assign_feed: @feed)
 		if @subscription.valid?
 			if request.xhr?
 				render partial: 'subscriptions/subscription', locals: { section: @section, subscription: @subscription }
